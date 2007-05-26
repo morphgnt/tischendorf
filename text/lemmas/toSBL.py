9f4d741d9125b11f04457c0df72f1b4464871733
Ulrik Sandborg-Petersen <ulrikp@emdros.org>
2007-05-26 15:21:27 +0000

    Now outputs in a simpler format.

--- text/lemmas/toSBL.py
+++ text/lemmas/toSBL.py
@@ -8,4 +8,4 @@ for line in sys.stdin.readlines():
     lemma_arr = arr[1].split(u" ! ")
     lemma_arr_utf8 = [convert.beta2sbl(x) for x in lemma_arr]
 
-    print (u"%s : %s" % (arr[0], " ! ".join(lemma_arr_utf8))).encode('utf-8')
+    print (u"%s %s" % (arr[0], lemma_arr_utf8[0])).encode('utf-8')
