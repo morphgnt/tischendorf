8226827051dae08f68b1b2da0f010df57a6d5fc1
Ulrik Sandborg-Petersen <ulrikp@emdros.org>
2007-05-26 14:42:19 +0000

    Initial addition.

---
+++ text/lemmas/toSBL.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import sys
+from morphgnt import convert
+
+for line in sys.stdin.readlines():
+    line = line.decode('utf-8').strip()
+    arr = line.split(u" : ")
+    lemma_arr = arr[1].split(u" ! ")
+    lemma_arr_utf8 = [convert.beta2sbl(x) for x in lemma_arr]
+
+    print (u"%s : %s" % (arr[0], " ! ".join(lemma_arr_utf8))).encode('utf-8')
