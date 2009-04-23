b0392b661c0cc1aec6674c9149f849718fd598ac
Ulrik Sandborg-Petersen <ulrikp@emdros.org>
2009-04-23 22:50:28 +0000

    Updated to be in sync with markind.py.

--- code/kind.py
+++ code/kind.py
@@ -4,10 +4,17 @@ kind_parsing = 2
 kind_pipe = 3
 kind_parens = 4
 kind_unknown = 5
+kind_verse = 6
+kind_VAR = 7
+kind_END = 8
+kind_OMIT = 9
 
 def recognize(str):
     if str[0] in "0123456789":
-        return kind_number
+	if ":" in str:
+	    return kind_verse
+	else:
+	    return kind_number
     elif str[0] == "|":
         return kind_pipe
     elif str[0] == "{":
@@ -16,6 +23,12 @@ def recognize(str):
         return kind_parens
     elif str[0] in "abcdefghihjklmnopqrstuvwxyz[]<>":
         return kind_word
+    elif str == "VAR:":
+	return kind_VAR
+    elif str == ":END":
+	return kind_END
+    elif str == "OMIT":
+	return kind_OMIT
     else:
         return kind_unknown
 
